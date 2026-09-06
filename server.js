2d31d825634c389486ded2fd742aac6ef939ee68
blackdiamond <blackdiamond.biznes@gmail.com>
2026-09-06 19:56:51 +0500

    admin: Telegram WebApp + no-cache admin routes

--- server.js
+++ server.js
@@ -109,14 +109,18 @@ const authLimiter = rateLimit({
 
 app.use('/api/', apiLimiter);
 app.use(express.json({ limit: '1mb' }));
-app.use(express.static(path.join(__dirname, 'public')));
-
 app.get('/admin', (req, res) => {
+  res.set('Cache-Control', 'no-store');
+  res.sendFile(path.join(__dirname, 'public', 'admin.html'));
+});
+app.get('/admin.html', (req, res) => {
+  res.set('Cache-Control', 'no-store');
   res.sendFile(path.join(__dirname, 'public', 'admin.html'));
 });
 app.get('/mijoz', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'mijoz.html'));
 });
+app.use(express.static(path.join(__dirname, 'public')));
 
 function safeEqualString(a, b) {
   const ba = Buffer.from(String(a), 'utf8');
